d30b5b30a3ff9745fa44f6981200688f217477b8
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-02-16 12:54:38 +0530

    Error Handling for Places routes!

--- routes/places.js
+++ routes/places.js
@@ -1,23 +1,37 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
 const DUMMY_PLACES = require("./dummy_data/dummy_places");
 
 const placesRoutes = express.Router();
 
 placesRoutes.get("/", function placesHomeRoute(req, res, next) {
   const places = DUMMY_PLACES;
+  if (!places) {
+    return next(new HttpError("Could not find any places.", 404));
+  }
   res.json({ places });
 });
 
 placesRoutes.get("/:placeId", function placesHomeRoute(req, res, next) {
   const { placeId } = req.params;
   const place = DUMMY_PLACES.find(p => p.id === placeId);
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided `placeId`.", 404),
+    );
+  }
   res.json({ place });
 });
 
 placesRoutes.get("/user/:userId", function placesHomeRoute(req, res, next) {
   const { userId } = req.params;
   const place = DUMMY_PLACES.find(p => p.creator === userId);
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided `userId`.", 404),
+    );
+  }
   res.json({ place });
 });
 
