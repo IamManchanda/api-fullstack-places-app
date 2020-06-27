ba2bd29907830fd3eb6675e276af9c4dff4f0585
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-06-27 16:39:42 +0530

    Backend: Using Multer to Save Files

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const { readAllUsers, signupUser, loginUser } = require("../controllers/users");
 const { signupUserValidation } = require("../validators/users");
+const fileUpload = require("../middlewares/file-upload");
 
 const usersRoutes = express.Router();
 usersRoutes.get("/", readAllUsers);
-usersRoutes.post("/signup", signupUserValidation, signupUser);
+usersRoutes.post(
+  "/signup",
+  fileUpload.single("image"),
+  signupUserValidation,
+  signupUser,
+);
 usersRoutes.post("/login", loginUser);
 
 module.exports = usersRoutes;
