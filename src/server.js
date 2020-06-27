4a835154f5e32359c3c5e2715dc7c84b49107890
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-06-27 17:49:46 +0530

    File Upload: Wiring Backend with Frontend!

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(function errorHandlerForRoutableRequest(error, req, res, next) {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
+  res.status(typeof error.code === "number" ? error.code : 500);
   res.json({ message: error.message || "An unknown error occurred." });
 });
 
