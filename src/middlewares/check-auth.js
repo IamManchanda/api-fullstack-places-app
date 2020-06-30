20e6a293514af2c4a534b58d07591b9aaf2f0808
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-06-30 18:55:35 +0530

    Backend: Using & Attaching JWT (Tokens) in React

--- src/middlewares/check-auth.js
+++ src/middlewares/check-auth.js
@@ -5,6 +5,9 @@ const HttpError = require("../models/http-error");
 const secretKey = "supersecret_dont_share";
 
 const checkAuth = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
